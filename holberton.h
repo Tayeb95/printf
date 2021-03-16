a7468b2d568a4b69db2756519447e38321c8ae76
Tayeb95 <2604@holbertonschool.com>
2021-03-16 22:08:06 +0000

    header file

---
+++ holberton.h
@@ -0,0 +1,22 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdarg.h>
+/**
+  * struct op - contains print operators and their correspong functions
+  * @s: print operator
+  * @f: print function
+  */
+typedef struct op
+{
+	char *t;
+	int (*f)(va_list a);
+} ops;
+int _printf(const char *format, ...);
+int print(char c);
+int printchar(va_list a);
+int printstring(va_list a);
+int printint(va_list a);
+int printbinary(va_list a);
+int revstring(va_list a);
+int printrot13(va_list a);
+#endif
